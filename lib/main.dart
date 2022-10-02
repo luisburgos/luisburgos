66a2140c9acdc26eba320fa86bc7673e42a920dc
Luis Burgos <luisarturo.burgos@gmail.com>
2022-10-02 12:51:09 -0500

    refactor: rename main app widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,11 @@ import 'landing.dart';
 import 'landing_project.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WebsiteApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WebsiteApp extends StatelessWidget {
+  const WebsiteApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
